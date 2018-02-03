9e01de6116fac54c775676966a7396a2790298e9
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2018-02-03 17:01:04 -0500

    Update PolyParkBeta1.ino

--- Arduino/PolyParkBeta1/PolyParkBeta1.ino
+++ Arduino/PolyParkBeta1/PolyParkBeta1.ino
@@ -1,6 +1,7 @@
 
 //Polypark Beta Arduino Code !!!!
 
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <ArduinoJson.h>
@@ -12,61 +13,63 @@ byte ip[] = { 192, 168, 0, 112 };                   // ip in lan can change
 byte subnet[] = { 255, 255, 255, 0 };              //subnet mask
 byte gateway[] = { 192, 168, 0, 1 };              // default gateway
 EthernetServer server(80);                       //server port
-const int trigPin = 3;
+
+/********************* ARDUINO SETTINGS ********************/
+const int trigPin = 3; // For the ultrasonic.
 const int echoPin = 4;
-long duration;// For real spot
-int distance;
-String status; // Status of the spot OPEN or CLOSED
+const int ledPinRed = 5; // Red in pin 5.
+const int ledPinGreen = 6;
+unsigned long previousMillis = 0;  // Error handler .
+const long LedTime = 1000; // Change to change the LED setting Error collection, if not set, if errors occur, leds flash.
+long duration; // Some math involved, tells distances using duration of sound.
+int distance; // Ultrasonic again No need to be exact.
+String status = "NULL"; // Parkling lot status, empty or full. Should never be null.
+int prevDistance;
+
+
 void setup()
 {
 Ethernet.begin(mac,ip,gateway,subnet);     // initialize Ethernet device
 server.begin();                                // start to listen for clients
 pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT);                            // input pin for switch
-Serial.begin(9600);
+Serial.begin(9600);           // Debugger can remove later to save resources. 
 }
 
 
 
 void loop()
 {
-
+  // Begin server at IP
 EthernetClient client = server.available();
 client.println("HTTP/1.1 200 OK");
 client.println("Content-Type: text/html");
 client.println("Connnection: close");
 client.println();
-// Clears the trigPin
 
-// All for sensing distance//
-
+// ************************ ULTRASONIC CONTROLLER *******************************/
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
-
-
-// Sets the trigPin on HIGH state for 10 micro seconds
 digitalWrite(trigPin, HIGH);
 delayMicroseconds(10);
 digitalWrite(trigPin, LOW);
+
+// Light controller function, Also sends Error checked status to globals.
+StatusLight();
 
 
 // Reads the echoPin, returns the sound wave travel time in microseconds
 duration = pulseIn(echoPin, HIGH);
 // Calculating the distance
 distance = duration*0.034/2;
-
-
+// Error checking to see if distance is too different from old, example random 3000 errors
+distanceError();
+prevDistance = distance; // Error corrector in arduino, easier than in HTML.
 // Prints the distance on the Serial Monitor
 // Debug
 Serial.print("Distance: ");
 Serial.println(distance);
 
-//check to export, can change to function if needed  
-  
-if (distance < 50 ){
-  status = "CLOSED";
-}
-else{status= "OPEN";} 
 
 // JSON Creator
 StaticJsonBuffer<200> jsonBuffer;
@@ -83,15 +86,43 @@ object4["Dummy3"] = randomnum();
 root.printTo(client);
 
 
-client.println("<meta http-equiv=\"refresh\" content=\"10\">"); // Refresh data ever 10 sec can cahnge later if needed.
+client.println("<meta http-equiv=\"refresh\" content=\"10\">"); // Refresh data every 10 sec can cahnge later if needed.
 
 
 
 client.stop(); // Stop the connection so it wont keep trying to connect to the server.
 }
 
-String randomnum(){ // Dummy creator
+String randomnum(){
  int randomnum = random(1,3);
   if(randomnum == 2){return "CLOSED";}
   else{return "OPEN";}
+  }
+
+void StatusLight(){
+  unsigned long currentMillis = millis(); // For led controller
+
+if (distance < 50 && currentMillis - previousMillis >= LedTime ){
+  
+  status = "CLOSED";
+    digitalWrite(ledPinGreen, LOW);
+   digitalWrite(ledPinRed, HIGH);
+   previousMillis = currentMillis;
+
+}
+else if (currentMillis - previousMillis >= LedTime) {status= "OPEN";
+digitalWrite(ledPinRed, LOW);
+digitalWrite(ledPinGreen, HIGH);
+  previousMillis = currentMillis;
+
+} // data = status, didnt wanna reuse vars  
+  
+  }
+
+void distanceError(){
+
+for (int i=0; i < 3; i++){// We can assume if 3 errors in a row, data is actually correct    
+  if (distance - 1000 > prevDistance){distance = prevDistance;} // Distance is 30 - 1000 prev is 30
+ 
+}
   }
